0da20f8954f8d775e38ee045f9aebc134e4cb5d2
mieubrisse <mieubrisse@gmail.com>
2023-04-30 23:46:03 -0300

    Added a highlightable list

--- components/highlightable_list/highlightable_list.go
+++ components/highlightable_list/highlightable_list.go
@@ -1,1 +1,43 @@
 package highlightable_list
+
+import (
+	"github.com/mieubrisse/teact/components/list"
+	"github.com/mieubrisse/teact/utilities"
+)
+
+type HighlightableList[T HighlightableComponent] interface {
+	list.List[T]
+
+	// Scrolls the highlighted item, with safeguards to prevent scrolling off the end of the list
+	Scroll(offset int) HighlightableList[T]
+}
+
+type impl[T HighlightableComponent] struct {
+	list.List[T]
+
+	highlightedIdx int
+}
+
+func New[T HighlightableComponent]() HighlightableList[T] {
+	return &impl[T]{
+		List: list.New[T](),
+	}
+}
+
+func (i impl[T]) Scroll(offset int) HighlightableList[T] {
+	newHighlightedIdx := utilities.Clamp(i.highlightedIdx, 0, len(i.List.GetItems())-1)
+	if i.highlightedIdx == newHighlightedIdx {
+		return i
+	}
+
+	items := i.List.GetItems()
+	if len(items) == 0 {
+		return i
+	}
+
+	items[i.highlightedIdx].SetHighlight(false)
+	items[newHighlightedIdx].SetHighlight(true)
+
+	i.highlightedIdx = newHighlightedIdx
+	return i
+}
